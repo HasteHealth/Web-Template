b003bb6ef0f6eacaf0a4ef8093889ee56a9f04bd
confused-moniker <ralph@iguhealth.app>
2026-01-26 12:18:02 -0600

    mod

--- app/routes/Patient.tsx
+++ app/routes/Patient.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import type { Route } from "./+types/home";
-import type { Bundle, Patient } from "@haste-health/fhir-types/r4/types";
+import type {
+  AllergyIntolerance,
+  Bundle,
+  Patient,
+} from "@haste-health/fhir-types/r4/types";
 
 import {
   // FHIRGenerativeSearchTable,
@@ -13,6 +17,36 @@ export function meta({}: Route.MetaArgs) {
     { title: "Haste Health Web App" },
     { name: "description", content: "Example web app for Haste Health" },
   ];
+}
+
+function capitalize(s: string) {
+  return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
+function AllergyDisplay({
+  allergies,
+}: {
+  allergies: Record<string, AllergyIntolerance[]>;
+}) {
+  return (
+    <div className="p-4 border space-y-2">
+      <div>
+        <span className="text-xl underline">Allergies</span>
+      </div>
+      {Object.keys(allergies).map((category) => (
+        <div key={category}>
+          <div>
+            <span className="text-lg underline">{capitalize(category)}</span>
+          </div>
+          <ul className="text-sm">
+            {allergies[category].map((allergy) => {
+              return <li key={allergy.id}>{allergy.code?.text}</li>;
+            })}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default function Home({ params }: Route.ComponentProps) {
@@ -61,6 +95,12 @@ export default function Home({ params }: Route.ComponentProps) {
               url: `Patient/${params.pid}/Medication`,
             },
           },
+          {
+            request: {
+              method: "GET",
+              url: `Patient/${params.pid}/AllergyIntolerance`,
+            },
+          },
         ],
       } as Bundle)
       .then((bundle) => {
@@ -68,12 +108,19 @@ export default function Home({ params }: Route.ComponentProps) {
       });
   }, [params.pid]);
 
-  let patient = patientData?.entry?.[0]?.resource as unknown as
+  const patient = patientData?.entry?.[0]?.resource as unknown as
     | Patient
     | undefined;
 
+  const allergies: Record<string, AllergyIntolerance[]> = Object.groupBy(
+    ((
+      patientData?.entry?.[6]?.resource as unknown as Bundle | undefined
+    )?.entry?.map((e) => e.resource) as unknown as AllergyIntolerance[]) ?? [],
+    (a: AllergyIntolerance) => a.category?.[0] ?? "",
+  );
+
   return (
-    <main>
+    <main className="space-y-8">
       <div className="border p-4 shadow-sm ">
         <div className="flex space-x-4">
           <div>
@@ -85,10 +132,13 @@ export default function Home({ params }: Route.ComponentProps) {
           <div>
             <div className="font-light text-sm underline">Last Name</div>
             <div>
-              <span>{patient?.name?.[0]?.family}</span>
+              <span>{patient?.name?.[0]?.family} </span>
             </div>
           </div>
         </div>
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        <AllergyDisplay allergies={allergies} />
       </div>
     </main>
   );
